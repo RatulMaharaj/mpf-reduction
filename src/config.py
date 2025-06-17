e217a8a7215307b168e0723ad94acea0edb93f13
Ratul Maharaj <RMaharaj3@oldmutual.com>
2025-06-17 12:57:50 +0200

    Update config

--- src/config.py
+++ src/config.py
@@ -1,12 +1,5 @@
 from pathlib import Path
 
-input_dir = Path(
-    R"\\OMRPRTP01.za.omlac.net\PRODUCTION\PE_Results\OM1\MFC RSA\2024-12\Mass Protect"  # noqa
-)
-local_temp_dir = Path(R"C:\Temp\MPF_Files")
-out_dir = Path(
-    R"\\OMRPRTP05.za.omlac.net\DEVELOPMENT\PE_Results\Segments\MFC RSA\2024-12\Mass Risk\Ratul\OMP"  # noqa
-)
 is_omp = True
 use_local_copy = True
 
@@ -16,21 +9,29 @@ runs_of_interest = [
     "408",
     "413",
     "414",
+    "415",
     "418",
     "419",
     "420",
     "421",
     "422",
     "423",
-    "415",
-    "424",
-    "425",
-    "426",
-    "427",
-    "428",
-    "429",
-    "430",
-    "431",
-    "432",
-    "433",
 ]  # noqa
+
+
+input_dir = Path(
+    R"\\omrprtp05.za.omlac.net\DEVELOPMENT\PE_Results\Segments\MFC RSA\2024-12\Mass Risk\Full"  # noqa
+)
+local_temp_dir = Path(R"C:\Temp\MPF_Files")
+out_dir = Path(
+    R"\\OMRPRTP05.za.omlac.net\DEVELOPMENT\PE_Results\Segments\MFC RSA\2024-12\Mass Risk\Ratul"  # noqa
+)
+
+
+if is_omp:
+    input_dir = Path(
+        R"\\OMRPRTP01.za.omlac.net\PRODUCTION\PE_Results\OM1\MFC RSA\2024-12\Mass Protect"  # noqa
+    )
+    out_dir = Path(
+        R"\\OMRPRTP05.za.omlac.net\DEVELOPMENT\PE_Results\Segments\MFC RSA\2024-12\Mass Risk\Ratul\OMP"  # noqa
+    )
