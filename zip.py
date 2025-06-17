3a13c8bfb868d4542326356c9b37c9e44b81cf95
Ratul Maharaj <RMaharaj3@oldmutual.com>
2025-06-17 11:35:45 +0200

    Move to individual archives

--- zip.py
+++ zip.py
@@ -1,19 +1,20 @@
 # zip up all the files ending with csv in the out_dir
 import zipfile
-from src.config import out_dir, is_omp
+from src.config import out_dir
 
-# Get all files in the out_data folder
-files = list(out_dir.glob("*.csv"))
+# Get all combined files in the out_dir folder
+files = list(out_dir.glob("combined_*.csv"))
 
+# Create a zip file for each combined file
+for file in files:
+    # Create zip filename based on the original file name
+    zip_file_name = f"{file.stem}.zip"
+    zip_file_path = out_dir / zip_file_name
 
-zip_file_name = "omp_runs.zip" if is_omp else "vantage_runs.zip"
-zip_file_path = out_dir / zip_file_name
-
-# Create a zip file with compression (ZIP_DEFLATED)
-with zipfile.ZipFile(
-    f"{zip_file_path}",
-    "w",
-    compression=zipfile.ZIP_DEFLATED,
-) as zipf:
-    for file in files:
-        zipf.write(f"{zip_file_path}", file)
+    # Create a zip file with compression (ZIP_DEFLATED)
+    with zipfile.ZipFile(
+        f"{zip_file_path}",
+        "w",
+        compression=zipfile.ZIP_DEFLATED,
+    ) as zipf:
+        zipf.write(f"{zip_file_path}", file.name)
