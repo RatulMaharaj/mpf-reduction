bc771b78eedda5b20c2b5065121a52b5dab0edae
Ratul Maharaj <RMaharaj3@oldmutual.com>
2025-06-17 11:43:43 +0200

    Add some logs

--- zip.py
+++ zip.py
@@ -1,9 +1,11 @@
 # zip up all the files ending with csv in the out_dir
 import zipfile
 from src.config import out_dir
+from src.logger import logger
 
 # Get all combined files in the out_dir folder
 files = list(out_dir.glob("combined_*.csv"))
+logger.info(f"Found {len(files)} combined CSV files to compress")
 
 # Create a zip file for each combined file
 for file in files:
@@ -18,4 +20,7 @@ for file in files:
         compression=zipfile.ZIP_DEFLATED,
         compresslevel=9,  # Maximum compression level
     ) as zipf:
+        logger.info(f"Compressing {file.name}")
         zipf.write(file, file.name)
+
+    logger.info(f"Compressed {file.name} to {zip_file_path}")
